592627550c5304d253769689ad9922b7d27184f5
nnhau1311 <nguyenngochau.iuh1311@gmail.com>
2021-09-26 14:58:49 +0700

    SpringBoot JWT by NguyenNgocHau

---
+++ src/test/java/com/example/spring_boot_jwt_nnh/SpringBootJwtNnhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_nnh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootJwtNnhApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
